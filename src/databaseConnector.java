b135148ec8e9c822aee927781397f5dc703411a0
Om Patel <patelomkumar472@gmail.com>
2023-08-04 16:41:20 +0530

    first commit

---
+++ src/databaseConnector.java
@@ -0,0 +1,26 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class databaseConnector {
+    final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    final static String DB_URL = "jdbc:mysql://localhost:3306/worker";
+    final static String USER = "root";
+    final static String PASS = "";
+    
+    public static Connection connection(){
+        
+            try{
+                
+                 Class.forName(JDBC_DRIVER);
+                 Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+                 return conn;
+            }catch(ClassNotFoundException | SQLException e){
+                JOptionPane.showMessageDialog(null,e);
+                return null;
+            }
+    }
+}
